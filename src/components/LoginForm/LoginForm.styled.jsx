5bcc1c9786036946cf3b905235fd6110d24b4e4f
Maksym Yatsyshyn <max.yatsushyn@gmail.com>
2023-09-14 09:27:19 +0300

    centered login

--- src/components/LoginForm/LoginForm.styled.jsx
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -1,28 +1,29 @@
 import styled from "@emotion/styled";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 // import TextField from '@mui/material/TextField';
 
 export const StyledLoginForm = styled.form`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    /* align-items: center; */
-    width: 320px;
-    margin-top: 30px;
-    margin-left: 40px;
+  display: flex;
 
-`
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 320px;
+  margin-top: 30px;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 export const LoginButton = styled(Button)`
-    width: fit-content;
-`
+  width: fit-content;
+`;
 
 export const RegisterInput = styled.input`
-    /* margin-bottom: 20px; */
-`
+  /* margin-bottom: 20px; */
+`;
 
 export const LoginLabel = styled.label`
-    display: flex;
+  display: flex;
   flex-direction: column;
   margin-bottom: 16px;
-`
+`;
